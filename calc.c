f50d54bcc9a81cd7aa51d8213294c1f383a03e58
gytanzo <bdm23@buffalo.edu>
2019-09-04 16:16:15 -0400

    If number is larger than 11 characters (12 for negatives), isinteger returns false

--- calc.c
+++ calc.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool isinteger(char *str);
 
 int parseint(char *str);
 
 int main(int argc, char *argv[]) {
-    char array[100]="100";
+    char array[100]="-2147483647";
 
     printf("%d \n", isinteger(array));
     return 0;
@@ -14,7 +15,13 @@ int main(int argc, char *argv[]) {
 
 bool isinteger(char *str){
     if (str[0] == '-')
-        return true;
+        if (strlen(str) < 12)
+            return true;
+        else
+            return false;
     else
-        return false;
+        if (strlen(str) < 11)
+            return true;
+        else
+            return false;
 }
