e7c352023d3927632f8fb552d36d441757e418a5
gytanzo <bdm23@buffalo.edu>
2019-09-04 18:41:30 -0400

    (Hopefully) got isinteger working

--- calc.c
+++ calc.c
@@ -7,15 +7,36 @@ bool isinteger(char *str);
 int parseint(char *str);
 
 int main(int argc, char *argv[]) {
-    char array[100]="-2147483647";
-
+    char *array="11231";
     printf("%d \n", isinteger(array));
     return 0;
 }
 
 bool isinteger(char *str){
-    if (str[0] == '-')
-        
-    else
-        
+    int i;
+    if (str[0] == '-'){
+        for(i = 1; str[i] != '\0'; i++){
+            if (isdigit(str[i])){
+                ;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    else{
+        for (i=0; str[i] != '\0'; i++){
+            if (isdigit(str[i])){
+                ;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int parseint(char *str){
+    
 }
