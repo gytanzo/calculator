67026aee9f9fde02121372c8a36fdd00e616c669
gytanzo <bdm23@buffalo.edu>
2019-09-04 16:08:09 -0400

    Added functionality to isinteger to check if negative

--- calc.c
+++ calc.c
@@ -6,8 +6,15 @@ bool isinteger(char *str);
 int parseint(char *str);
 
 int main(int argc, char *argv[]) {
+    char array[100]="100";
 
-
+    printf("%d \n", isinteger(array));
     return 0;
 }
 
+bool isinteger(char *str){
+    if (str[0] == '-')
+        return true;
+    else
+        return false;
+}
